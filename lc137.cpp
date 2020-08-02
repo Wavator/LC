adb2600c72f843d54f53aab297b6ec75775c9f7b
Wavator <zyzxjywy@163.com>
2020-08-02 22:16:05 +0800

    lc137,位运算

---
+++ lc137.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Zhao on 2020/8/2.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for (int i = 0; i < 32; ++i) {
+            int cnt = 0;
+            for (int& item: nums) {
+                cnt += (item >> i) & 1;
+            }
+            if (cnt % 3) {
+                res |= (1 << i);
+            }
+        }
+        return res;
+    }
+};
+
+
+/*
+给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现了三次。找出那个只出现了一次的元素。
+
+说明：
+
+你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+示例 1:
+
+输入: [2,2,3,2]
+输出: 3
+示例 2:
+
+输入: [0,1,0,1,0,1,99]
+输出: 99
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/single-number-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * */
